cf21a73788e3d5cdc8e6b5830f00f48cc8ef7a51
Sam Davies <sam@visualputty.co.uk>
2015-01-26 15:51:39 +0000

    Created a single view app demo for the photo extension

---
+++ Filtster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Filtster
+//
+//  Created by sam on 26/01/2015.
+//  Copyright (c) 2015 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
